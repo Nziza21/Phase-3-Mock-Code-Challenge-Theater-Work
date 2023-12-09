4299a70a1ac2ddef49885cf5053689422d014584
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-09 17:35:20 +0300

    changed

--- lib/seed.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, Audition, Role
-
-engine = create_engine('sqlite:///theater.db')
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
